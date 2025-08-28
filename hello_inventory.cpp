75dc392c3ffc8b0e7eb90c3e7f91502c54086534
RZendejasJr <rigoberto.zendejas@outlook.com>
2025-08-27 20:38:28 -0700

    Create hello_inventory.cpp

---
+++ hello_inventory.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print "Hello Inventory System!" and demonstrate escape characters
+    cout << "Hello Inventory System!\n";
+    cout << "\tData Types:\n";
+
+    // Output the sizes of int, float, and char
+    cout << "Size of int: " << sizeof(int) << " bytes\n";
+    cout << "Size of float: " << sizeof(float) << " bytes\n";
+    cout << "Size of char: " << sizeof(char) << " bytes\n";
+
+    return 0;
+}
